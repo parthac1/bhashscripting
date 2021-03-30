fb3556cf6c877f4cc6ad0eb3c8b0e5ead5b7ebd6
parthastudent <72043891+parthastudent@users.noreply.github.com>
2021-03-30 09:22:03 -0700

    1st branch changes

--- calulate.sh
+++ calulate.sh
@@ -3,14 +3,14 @@ read number1
 echo "Please Enter another number "
 read number2 
 
-addition=$number1+$number2
-multiplication=$number1*$number2
-# Declare a variable to perfrom substraction
+addition=$((number1+$number2))
+multiplication=(($number1*$number2))
+
 
 echo "Addition of the 2 numbers are $addition"
 
 echo " multiplication of the 2 numbers are $multiplication"
 
-# Print the Division result 
+
 
 
