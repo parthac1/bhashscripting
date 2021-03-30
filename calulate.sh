2c425a96768a9e4015028b722dcb17b74bc32cf8
parthastudent <72043891+parthastudent@users.noreply.github.com>
2021-03-30 09:17:53 -0700

    1st commit

---
+++ calulate.sh
@@ -0,0 +1,16 @@
+echo "Please Etner a number "
+read number1
+echo "Please Enter another number "
+read number2 
+
+addition=$number1+$number2
+multiplication=$number1*$number2
+# Declare a variable to perfrom substraction
+
+echo "Addition of the 2 numbers are $addition"
+
+echo " multiplication of the 2 numbers are $multiplication"
+
+# Print the Division result 
+
+
